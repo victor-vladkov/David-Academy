bf1f34a55c5a6c163630993322b971a05ebc34e6
Victor Vladkov <vvladkov2004@gmail.com>
2021-06-15 23:35:10 +0300

    Added discriminant exercise.

---
+++ 01. Discriminant/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _01._Discriminant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            double discriminant = Math.Pow(b, 2) - 4 * a * c;
+            Console.WriteLine(discriminant);
+        }
+    }
+}
