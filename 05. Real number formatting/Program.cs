c91079c32bafd2b3f86f4a63dd10d00fe2ef7ab4
Victor Vladkov <vvladkov2004@gmail.com>
2021-06-16 20:47:03 +0300

    Added real numbers exercise.

---
+++ 05. Real number formatting/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _05._Real_number_formatting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = double.Parse(Console.ReadLine());
+            int f = int.Parse(Console.ReadLine());
+            Console.WriteLine(n.ToString("F" + f));
+        }
+    }
+}
