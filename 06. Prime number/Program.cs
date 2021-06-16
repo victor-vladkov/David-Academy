e417442456d605ff5c1d52a1493ec10b0613f42d
Victor Vladkov <vvladkov2004@gmail.com>
2021-06-16 21:15:54 +0300

    Added prime number exercise.

---
+++ 06. Prime number/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _06._Prime_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 2; i < n / 2; i++)
+                if (n % i == 0) 
+                {
+                    Console.WriteLine("Числото не е просто.");
+                    return;
+                }
+            Console.WriteLine("Числото е просто.");
+        }
+    }
+}
