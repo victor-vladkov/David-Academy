18d24c04cddaacfec35ce8d84aa054e737c2f0b2
Victor Vladkov <vvladkov2004@gmail.com>
2021-06-16 20:46:45 +0300

    Added leap year exercise.

---
+++ 04. Leap year/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _04._Leap_year
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = int.Parse(Console.ReadLine());
+            if (year % 4 == 0)
+                Console.WriteLine("Годината е високосна.");
+            else 
+                Console.WriteLine("Годината не е високосна.");
+            //Console.WriteLine($"Годината {(int.Parse(Console.ReadLine()) % 4 == 0 ? "" : "не ")}e високосна.");
+        }
+    }
+}
