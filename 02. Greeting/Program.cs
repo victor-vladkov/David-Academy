d10af6ffe73fc9ec1868292cf5dad0c5f37b1ef8
Victor Vladkov <vvladkov2004@gmail.com>
2021-06-16 20:46:16 +0300

    Added Greeting exercise.

---
+++ 02. Greeting/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _02._Greeting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string studentName = Console.ReadLine();
+            string courseName = Console.ReadLine();
+            Console.WriteLine($"Здравейте, {studentName}, и добре дошли в {courseName}!");
+        }
+    }
+}
