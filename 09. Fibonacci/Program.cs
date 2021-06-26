164eb4b1e4a045440f902da27f934f8c641aa933
Victor Vladkov <vvladkov2004@gmail.com>
2021-06-27 01:34:00 +0300

    Added Fibonacci exercise.

---
+++ 09. Fibonacci/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _09._Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()),
+				c = 3;
+			int[] nums = new int[] { 1, 1, 2 };
+			while (c < n) {
+				nums[0] = nums[1];
+				nums[1] = nums[2];
+				nums[2] = nums[0] + nums[1];
+				c++;
+			}
+			Console.WriteLine(nums[2]);
+        }
+    }
+}
---
+++ 09. Fibonacci/README.md
@@ -0,0 +1,2 @@
+### Fibonacci numbers
+This console program finds the number in the Fibonacci order the position of which corresponds to the inserted number.
