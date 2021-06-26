74533b845cd95724e9f309f0b5637c415cda840d
Victor Vladkov <vvladkov2004@gmail.com>
2021-06-27 01:34:36 +0300

    Added quadratic equation exercise.

---
+++ 10. Quadratic equation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _10._Quadratic_equation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("A = ");
+			double a = double.Parse(Console.ReadLine());
+			Console.Write("B = ");
+			double b = double.Parse(Console.ReadLine());
+			Console.Write("C = ");
+			double c = double.Parse(Console.ReadLine()),
+			d = b * b - 4 * (a + c);
+			if (d < 0) {
+				Console.WriteLine("Negative discriminant!");
+				return;
+			}
+			double x1 = (-b + Math.Sqrt(d)) * .5,
+				x2 = (-b - Math.Sqrt(d)) * .5;
+			Console.WriteLine($"x1 = {x1:f2}\nx2 = {x2:f2}");
+        }
+    }
+}
---
+++ 10. Quadratic equation/README.md
@@ -0,0 +1,2 @@
+### Solutions to a quadratic equation
+This console program finds all real solutions to a quadratic equation with the inserted coefficients.
