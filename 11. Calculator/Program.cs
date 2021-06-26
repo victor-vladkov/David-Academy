0c50e2ac68463c107cdf496eea23be51a3f2cfc4
Victor Vladkov <vvladkov2004@gmail.com>
2021-06-27 01:35:38 +0300

    Added calculator exercise.

---
+++ 11. Calculator/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _11._Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Commands: add, sub, mul, div, exit");
+			string command = Console.ReadLine().ToLower();
+			while (command != "exit") {
+				switch (command) {
+					case "add":
+						Console.Write("A = ");
+						double a = double.Parse(Console.ReadLine());
+						Console.Write("B = ");
+						double b = double.Parse(Console.ReadLine());
+						Console.WriteLine("A + B = " + (a + b));
+						break;
+					case "sub":
+						Console.Write("A = ");
+						a = double.Parse(Console.ReadLine());
+						Console.Write("B = ");
+						b = double.Parse(Console.ReadLine());
+						Console.WriteLine("A - B = " + (a - b));
+						break;
+					case "mul":
+						Console.Write("A = ");
+						a = double.Parse(Console.ReadLine());
+						Console.Write("B = ");
+						b = double.Parse(Console.ReadLine());
+						Console.WriteLine("A * B = " + (a * b));
+						break;
+					case "div":
+						Console.Write("A = ");
+						a = double.Parse(Console.ReadLine());
+						Console.Write("B = ");
+						b = double.Parse(Console.ReadLine());
+						Console.WriteLine("A / B = " + (a / b));
+						break;
+				}
+				command = Console.ReadLine().ToLower();
+            }
+        }
+    }
+}
---
+++ 11. Calculator/README.md
@@ -0,0 +1,2 @@
+### Calculator
+This console program executes mathematical operations with the inserted numbers.
