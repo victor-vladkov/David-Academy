29b658b2ef66b42718e9e6adc1a33170dd692a3f
Victor Vladkov <vvladkov2004@gmail.com>
2021-07-07 00:01:42 +0300

    Added Inversions count exercise.

---
+++ 14. Inversions count/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _15._Inversions_count {
+	class Program {
+		static void Main(string[] args) {
+			int[] nums = Console.ReadLine()
+				.Split(" ")
+				.Select(int.Parse)
+				.ToArray();
+			int c = 0;
+			for (int i = 0; i < nums.Length - 1; i++)
+				for (int j = i + 1; j < nums.Length; j++)
+					if (i < j && nums[i] > nums[j])
+						c++;
+			Console.WriteLine(c);
+		}
+	}
+}
---
+++ 14. Inversions count/README.md
@@ -0,0 +1,2 @@
+### Inversions count
+This console program finds the number of inversions in an array inserted by the user.
