6a0626b6e2279a35faf9ef05986fd3c245001684
Victor Vladkov <vvladkov2004@gmail.com>
2021-07-07 00:00:43 +0300

    Added Minmax exercise.

---
+++ 12. MinMax/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _13._MinMax {
+	class Program {
+		static void Main(string[] args) {
+			int[] nums = Console.ReadLine()
+				.Split(" ")
+				.Select(int.Parse)
+				.ToArray();
+			int min = int.MaxValue,
+				max = int.MinValue;
+			double sum = 0;
+			for (int i = 0; i < nums.Length; i++) {
+				if (min > nums[i])
+					min = nums[i];
+				if (max < nums[i])
+					max = nums[i];
+				sum += nums[i];
+			}
+			Console.WriteLine($"Min: {min}\nMax: {max}\nAvg: {sum / nums.Length}");
+		}
+	}
+}
---
+++ 12. MinMax/README.md
@@ -0,0 +1,2 @@
+### Max, min and average values of array
+This console program finds the maximum, minimum and average values of an inserted array.
