2f50859beecdb6bb678da167e4295afd5b5beb6a
Victor Vladkov <vvladkov2004@gmail.com>
2021-07-13 23:44:22 +0300

    Added Zoo exercise.

---
+++ 102. Zoo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _102._Zoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Animal animal = new Animal(name);
+            Reptile reptile = new Reptile(name);
+            Mammal mammal = new Mammal(name);
+            Lizard lizard = new Lizard(name);
+            Snake snake = new Snake(name);
+            Gorilla gorilla = new Gorilla(name);
+            Bear bear = new Bear(name);
+            Console.WriteLine(animal, reptile, mammal, lizard, snake, gorilla, bear);
+        }
+    }
+}
